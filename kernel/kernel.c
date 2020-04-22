aafd7948b9a1592e38083797a7ab82d38c27b914
archer.li <minyakonga@gmail.com>
2020-04-22 16:41:26 +0800

    my simple screen driver is working

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,22 +1,6 @@
-void clean_screen() {
-    char* vid_mem = (char*) 0xb8000;
-    for (int idx = 0; idx < 25; idx++) {
-        for (int jdx = 0; jdx < 80; jdx++) {
-            *vid_mem = ' ';
-            vid_mem += 2;
-        }
-    }
-}
+#include "../drivers/screen.h"
 
 void main() {
-    // create a pointer to a char, and point it to the first text cell of
-    // vid mem: the top left of the screen
-    char* video_memory = (char*) 0xb8000;
-
-    clean_screen();
-    char welcome_msg[] = "Hello World From Clang";
-    for (int idx = 0; idx < 22; idx++) {
-        *video_memory = welcome_msg[idx];
-        video_memory += 2;
-    }
+    clear_screen();
+    kprint_at("Hello World From C!", 30, 12);
 }
